2944c0e3f67008cdf8c6334a9ad6a03738bb1847
bauidch <lukas.bauer@competec.ch>
2019-07-15 10:58:35 +0200

    Seperate routes

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	routes "github.com/bauidch/hyres-api/routers"
 )
 
 // Define HTTP request routes
 func main() {
-	router := mux.NewRouter()
-
+	router := routes.NewRouter()
+	log.Printf("Server started")
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Access-Control-Allow-Origin", "Content-Type", "Content-Range"})
 	originsOk := handlers.AllowedOrigins([]string{"http://hyres-app.bauid.ch:3000", "*"})
@@ -20,4 +20,5 @@ func main() {
 	if err := http.ListenAndServe(":8080",  handlers.CORS(originsOk, headersOk, methodsOk, exposeOK)(router)); err != nil {
 		log.Fatal(err)
 	}
+
 }
